4cf91ec858c9b816e91fab4ff92ef5586f26b504
Gabriel Nunes <74620108+gabrielnuunes@users.noreply.github.com>
2023-09-18 21:39:44 -0300

    Condicional IF primeiro dia

--- road-java/src/academy/roadjava/introducao/Aula05OperadoresCondicionais.java
+++ road-java/src/academy/roadjava/introducao/Aula05OperadoresCondicionais.java
@@ -1,4 +1,22 @@
 package academy.roadjava.introducao;
 
 public class Aula05OperadoresCondicionais {
+
+    private static String podeEntrar;
+
+    public static void main(String[] args) {
+
+        int idade = 15;
+        boolean isDeMaior = idade >= 18;
+        if(isDeMaior){
+            System.out.println("pode Entrar");
+        }
+        if(!isDeMaior){
+            System.out.println("Não pode entrar");
+        }
+
+
+        
+    }
+
 }
